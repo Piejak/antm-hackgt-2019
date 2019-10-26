2a020415716cf88250efbeff5c39d0609807dc20
Tim-Holloway52 <57024500+Tim-Holloway52@users.noreply.github.com>
2019-10-26 00:28:04 -0400

    Added terminology

---
+++ zazu/src/Terminology.js
@@ -0,0 +1,13 @@
+const EndString
+    EndText:"Click to learn more about"
+const FinTerms={
+    ETF: `Short for Exchange-Traded Fund. ETFs are made up of many different stocks, allowing you to get access to entire industries with only one trade.` + EndText + '<a href="https://www.blackrock.com/us/individual/education/ishares-etfs" > "ETFs." </a>',
+   
+    MutualFund: `A mutual fund is a group of stocks, bonds, and other assets that is managed by a professional. These professionals run the fund and choose the stocks for you based on the strategy of the fund.` + EndText + '<a href="https://www.blackrock.com/us/individual/education/mutual-funds" > "mutual funds." </a>',
+    
+    Bonds: "A bonds is you loaning your money to the government for a fixed period of time. That set time, the government pays you back the price of the bond plus the interest it gained. Bonds are usually considered a safer investment than stocks, as they're backed by the U.S government.",
+   
+    Portfolio: "A portfolio the name for a group of investments. A portfolio can consist of: stocks, bonds, ETFs, mutual funds, and many other assets.",
+
+    Equity: "An equity"
+}
