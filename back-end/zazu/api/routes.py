c8bb6822836510b3d21486dace0ab0f633839447
Brian Piejak <Brian.Piejak@gmail.com>
2019-10-26 21:21:55 -0400

    Implement passthrough route for performance and adds cors

--- back-end/zazu/api/routes.py
+++ back-end/zazu/api/routes.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from flask import request
+import requests
 
 from . import api_blueprint
 from ..util import get_normalized_prices, get_optimized_allocations
@@ -18,3 +19,9 @@ def get_allocation():
     optimized_allocations = get_optimized_allocations(tickers, normalized_prices_df)
 
     return optimized_allocations
+
+@api_blueprint.route('/performance', methods=['GET'])
+def get_performance():
+    portfolio = request.args.get('positions')
+    resp = requests.get('https://www.blackrock.com/tools/hackathon/portfolio-analysis?calculatePerformance=true&positions=' + portfolio)
+    return resp.content
